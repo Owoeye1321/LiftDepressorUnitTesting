a9684901fe8da64d906a3b3f4320a3efedbb8a92
Owoeye Samuel <owoeye1321@gmail.com>
2024-07-07 17:27:44 +0100

    holy dang! i am a genius on C#

---
+++ MotivatorServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LiftDepression.Interface;
+namespace LiftDepressionUnitTesting
+{
+	public class MotivatorServiceTest
+	{
+		private readonly IMotivatorAction _motivatorService;
+		public MotivatorServiceTest()
+		{
+			_motivatorService = 
+		}
+
+
+	}
+}
+
