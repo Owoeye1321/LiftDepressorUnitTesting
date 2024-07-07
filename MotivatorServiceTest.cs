9003cd944d50e0430cbc55449b5ecd5a2129296c
Owoeye Samuel <owoeye1321@gmail.com>
2024-07-07 17:32:40 +0100

    the really fuck i am talking about! oh lad

--- MotivatorServiceTest.cs
+++ MotivatorServiceTest.cs
@@ -1,16 +1,40 @@
 ﻿using System;
+using FakeItEasy;
+using LiftDepression.Controllers;
 using LiftDepression.Interface;
 namespace LiftDepressionUnitTesting
 {
 	public class MotivatorServiceTest
 	{
 		private readonly IMotivatorAction _motivatorService;
+
 		public MotivatorServiceTest()
 		{
-			_motivatorService = 
+			//dependencies
+			_motivatorService = A.Fake<IMotivatorAction>();
+
+			//classes
+			//var motivatorController = new MotivatorController(_motivatorService);
+
 		}
 
 
-	}
+		[Fact]
+
+		public void MotivatorController_GetAllQuotes_ReturnsIEnumerableIGetMotivationResponse()
+		{
+			//Arrange
+
+
+			//Act
+			var result = _motivatorService.GetAllQuotes();
+
+
+			//Assert
+		}
+
+
+
+    }
 }
 
